9aca9146392cff5eb1e0e684b2d469ac621272c5
Sérgio Alves <smsa.prog@gmail.com>
2023-11-25 10:38:00 -0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<!--canoa.2018-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>IGR</title>
+         
+        <link rel="stylesheet" type="text/css" href= "css/estilo.css"/>
+        <?php include_once 'classes/Conexao.php';?>
+    </head>
+    <body>
+           <?php include_once 'estrutura/header.php';?> 
+        <main  >
+            <article>
+            <?php include_once 'estrutura/login_main.php';?>   
+            </article>  
+        </main>
+        <footer>  <hr align="left" width="100%" size="1" color='#848484'>
+            <p>Desenvolvido por smsa&#169 - 2018 smsa.prog@gmail.com</p><br> 
+       <img src="estrutura/img/myadmin.jpg" style="width: 120px;height:100px;">
+       <img src="estrutura/img/php.png" style="width: 150px;height: 100px;">
+       <img src="estrutura/img/xampp.jpg" style="width: 120px;height: 100px;">
+        </footer>
+    </body>
+</html>
